699a898aafd5ae2d04570cd1be64e2a29956e988
qianyan <1550529084@qq.com>
2026-03-10 11:26:41 +0800

    add guard

---
+++ middleware.ts
@@ -0,0 +1,33 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export const config = {
+  matcher: ["/", "/:path*"], // 保护所有路由
+};
+
+export function middleware(req: NextRequest) {
+  if (process.env.NODE_ENV === "development") return NextResponse.next();
+
+  const basicAuth = req.headers.get("authorization");
+  const url = req.nextUrl;
+
+  if (basicAuth) {
+    const authValue = basicAuth.split(" ")[1];
+    const [user, pwd] = atob(authValue).split(":");
+
+    if (
+      user === process.env.BASIC_AUTH_USER &&
+      pwd === process.env.BASIC_AUTH_PASSWORD
+    ) {
+      return NextResponse.next();
+    }
+  }
+
+  url.pathname = "/api/auth";
+
+  return new NextResponse("Auth Required", {
+    status: 401,
+    headers: {
+      "WWW-Authenticate": 'Basic realm="Secure Area"',
+    },
+  });
+}
